22b46e8d1f7956a38817a4ba17ced7d2c8ebf26d
carlos <carloszaragozabeato15@gmail.com>
2025-11-08 11:03:43 +0100

    first up

---
+++ src/main/java/com/carloszaragoza/ZtRunApplication.java
@@ -0,0 +1,13 @@
+package com.carloszaragoza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtRunApplication.class, args);
+	}
+
+}
