706ddb45a45e6e40cde32411df4f61014a3ed239
Carlos Zaragoza Beato <carlos@Carlos.home>
2025-11-10 12:04:45 -0500

    s

--- backend/src/main/java/com/carloszaragoza/ztrun/infrastructure/repository/auth/JpaUserRepository.java
+++ backend/src/main/java/com/carloszaragoza/ztrun/infrastructure/repository/auth/JpaUserRepository.java
@@ -3,7 +3,8 @@ package com.carloszaragoza.ztrun.infrastructure.repository.auth;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsername(String username);
-}
+
+//@Repository
+//public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+//    Optional<UserEntity> findByUsername(String username);
+//}
