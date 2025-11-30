77cc778937f1a10415d3e1e54d4cf3a6ab585b4f
Carlos Zaragoza Beato <carlos@Carlos.home>
2025-11-30 01:42:44 -0500

    test

--- field/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require("plugins")      -- primero plugins
-require("nvimtree")     -- carga configuración de nvim-tree
-require("settings")     -- tus ajustes, tema, etc.
-require("keymaps")      -- keymaps, incluye el atajo para toggle
-require("lsp")
-
